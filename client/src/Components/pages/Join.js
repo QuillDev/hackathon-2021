ab4f1447d2f3d48c83bcc7f1631c1f381dc1da21
Michael Erickson <erickson.michael2016@gmail.com>
2021-01-23 15:16:28 -0500

    added join.js

---
+++ client/src/Components/pages/Join.js
@@ -0,0 +1,15 @@
+import {Component} from "react";
+
+class Join extends Component{
+    render(){
+        return(
+            <div className={"App"}>
+                <header className={"App-header"}>
+
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Join;
