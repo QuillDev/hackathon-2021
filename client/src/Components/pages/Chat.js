ceb246ce990613d80cc35992f67955b9382d2fdd
Michael Erickson <erickson.michael2016@gmail.com>
2021-01-23 14:48:03 -0500

    added hover, avatars to chat.js

--- client/src/Components/pages/Chat.js
+++ client/src/Components/pages/Chat.js
@@ -1,25 +1,39 @@
 import {Component} from "react";
 import {Link} from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import avatar from "../../avatar.jpg";
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
+
+const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+        Michael Erickson
+    </Tooltip>
+);
 
 class Chat extends Component{
     render(){
         return(
             <div className={"App"}>
-                <header className={"App-header"} style={{alignItems:"flex-start", flexDirection:"row"}}>
+                <header className={"App-header-fix"}>
+                    <div style={{alignItems:"flex-start", flexDirection:"row"}}>
+                        <div style={{paddingBottom:"20px", paddingTop:"10px"}}>
+                            <a style={{borderStyle:"solid", fontSize:"40px", padding:"15px"}}>Michael's Chat Room</a>
+                        </div>
+                    </div>
                     <div style={{position:"absolute", left:"0px", width:"10px"}}>
-                        <circle></circle>
+                        <OverlayTrigger
+                            placement="right"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={renderTooltip}
+                        >
+                            <img src={avatar} style={{borderRadius:"50%", width:"85px", height:"85px"}}></img>
+                        </OverlayTrigger>
+                        <img src={avatar} style={{borderRadius:"50%", width:"85px", height:"85px"}}></img>
+                        <img src={avatar} style={{borderRadius:"50%", width:"85px", height:"85px"}}></img>
+                        <img src={avatar} style={{borderRadius:"50%", width:"85px", height:"85px"}}></img>
+                        <img src={avatar} style={{borderRadius:"50%", width:"85px", height:"85px"}}></img>
                     </div>
-                    <div style={{paddingBottom:"20px", paddingTop:"10px"}}>
-                        <a style={{borderStyle:"solid", fontSize:"40px", padding:"15px"}}>Michael's Chat Room</a>
-                    </div>
-                    <div>
-                        <Link to={"/"}>
-                            <Button className="btn" variant="dark">
-                                Wowee
-                            </Button>
-                        </Link>
-                    </div>
+
                 </header>
             </div>
         );
