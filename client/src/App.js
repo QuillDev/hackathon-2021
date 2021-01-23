cceea0ea4e948cbf75d78f5ddf69e5826ee7a5af
Robert Brunson <siinspam@gmail.com>
2021-01-23 12:22:49 -0500

    added api

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,49 @@ import logo from './logo.png';
 import './App.css';
 import Login from "./GoogleLogin/Login";
 import Logout from "./GoogleLogin/Logout";
-import {Button, Jumbotron} from "react-bootstrap";
+import {Jumbotron} from "react-bootstrap";
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-        <p>Michael's Chat Room</p>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: ""};
+  }
 
-        <Jumbotron>
-          <h1>Observe</h1>
-          <img src={logo} alt="Logo" />
-          <p>
-            A comprehensive solution to online tutoring.
-          </p>
-        </Jumbotron>
-        {/* Login and Logout Buttons */}
-        <Login/>
-        <Logout/>
-      </header>
-    </div>
-  );
+  callAPI() {
+    fetch("http://localhost:3069/api")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render(){
+    return (
+        <div className="App">
+          <header className="App-header">
+
+            <p>Michael's Chat Room</p>
+
+            <Jumbotron>
+              <h1>Observe</h1>
+              <img src={logo} alt="Logo" />
+              <p>
+                A comprehensive solution to online tutoring.
+              </p>
+            </Jumbotron>
+            {/* Login and Logout Buttons */}
+            <Login/>
+            <Logout/>
+
+            <p>{this.state.apiResponse}</p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
