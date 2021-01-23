77809607b2e01ae90e3d51c7effe928e879ef1df
Robert Brunson <siinspam@gmail.com>
2021-01-23 12:30:18 -0500

    style change

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
                 A comprehensive solution to online tutoring.
               </p>
             </Jumbotron>
-            <div style={{paddingTop:"10px"}}></div>
+            <div style={{paddingTop: "10px"}}/>
             <Login/>
           </header>
         </div>
