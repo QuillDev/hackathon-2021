2586e268b3f6c7a45510083e792f82d2382332dc
Michael Erickson <erickson.michael2016@gmail.com>
2021-01-23 22:26:56 -0500

    prettification

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import {Component} from "react";
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import logo from "../nonAnimatedLogo.gif"
 
 class App extends Component {
     render(){
         return (
-            <p>test</p>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Nav.Item as="img" src={logo} style={{width:"40px", height:"40px", marginRight:"10px"}}></Nav.Item>
+                <Navbar.Brand href="#home">Observe</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                </Navbar.Collapse>
+                <Nav>
+                    <!-- add profile photo/logout and name here-->
+                </Nav>
+            </Navbar>
         );
     }
 }
